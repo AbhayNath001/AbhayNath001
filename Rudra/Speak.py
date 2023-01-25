40dd2d293def29b754975ca45cbebb84111c93bb
Abhay Nath <103759014+AbhayNath001@users.noreply.github.com>
2023-01-25 15:37:07 +0530

    Add files via upload

---
+++ Rudra/Speak.py
@@ -0,0 +1,22 @@
+import pyttsx3
+
+#speak
+
+def Say(Text):
+    engine = pyttsx3.init('sapi5')
+    voices = engine.getProperty('voices')
+    engine.setProperty('voice', voices[0].id)
+    engine.setProperty('rate',170)
+    print(f"Rudra: {Text}")
+    engine.say(text=Text)
+    engine.runAndWait()
+    print(" ")  
+    
+def Just_Say(Text):
+    engine = pyttsx3.init('sapi5')
+    voices = engine.getProperty('voices')
+    engine.setProperty('voice', voices[0].id)
+    engine.setProperty('rate',170)
+    engine.say(text=Text)
+    engine.runAndWait() 
+    print(" ")     
