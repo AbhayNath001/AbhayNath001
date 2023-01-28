03b8c636330eaf019b360b4eeaffe0d29a8cc0db
Abhay Nath <103759014+AbhayNath001@users.noreply.github.com>
2023-01-28 14:23:27 +0530

    Add files via upload

---
+++ Rudra/re.py
@@ -0,0 +1,26 @@
+import cv2
+import numpy as np
+import pyautogui
+
+# set the screen resolution
+width, height = 1920, 1080
+
+# set the codec for the video
+fourcc = cv2.VideoWriter_fourcc(*"XVID")
+
+# create a video writer object
+out = cv2.VideoWriter("screen_recording.avi", fourcc, 20.0, (width, height))
+
+print("Started recording...")
+# start the screen recording
+while True:
+    img = np.array(pyautogui.screenshot())
+    frame = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    out.write(frame)
+    if cv2.waitKey(1) == ord("q"):
+        break
+
+print("Stopped recording.")
+# release the video writer and close the window
+out.release()
+cv2.destroyAllWindows()
