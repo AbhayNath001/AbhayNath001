6efe99b10b154b1c9ec75cdcbdd05ab7178f5261
Abhay Nath <103759014+AbhayNath001@users.noreply.github.com>
2023-02-28 01:16:56 +0530

    Update nasa.py

--- Rudra/nasa.py
+++ Rudra/nasa.py
@@ -15,15 +15,28 @@ try:
     neo = Data['near_earth_objects']
     print(f"\nTotal Astroids between from {start_date} to {end_date} is: {Total_Astro}")
     magnitudes = []
+    max_magnitude = float('-inf')
+    min_magnitude = float('inf')
+    max_asteroid_name = ""
+    min_asteroid_name = ""
     for body in neo[start_date]:
         name = body['name']
         absolute = body['absolute_magnitude_h']
         magnitudes.append(absolute)
+        if absolute > max_magnitude:
+            max_magnitude = absolute
+            max_asteroid_name = name
+        if absolute < min_magnitude:
+            min_magnitude = absolute
+            min_asteroid_name = name
         print(f"\nName: {name} \n\tAbsolute Magnitude: {absolute}")
     if magnitudes:
-        max_magnitude = max(magnitudes)
-        min_magnitude = min(magnitudes)
         print(f"\nMaximum absolute magnitude: {max_magnitude}")
+        print(f"Corresponding asteroid name: {max_asteroid_name}")
         print(f"Minimum absolute magnitude: {min_magnitude}")
+        print(f"Corresponding asteroid name: {min_asteroid_name}")
+    else:
+        print("No asteroids found.")
 except:
     print("Be sure the difference between starting date and ending date is not more than 7 days.")
+
