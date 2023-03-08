240d539d75dee0408d196077e6916fe7ba77b5a9
Abhay Nath <103759014+AbhayNath001@users.noreply.github.com>
2023-03-08 15:39:18 +0530

    Add files via upload

---
+++ backtotop.js
@@ -0,0 +1,39 @@
+                const showOnPx = 100;
+                const backToTopButton = document.querySelector(".back-to-top")
+
+                const scrollContainer = () => {
+                return document.documentElement || document.body;
+            };
+            
+            document.addEventListener("scroll", () => {
+            if (scrollContainer().scrollTop > showOnPx) {
+            backToTopButton.classList.remove("hidden")
+            } else {
+            backToTopButton.classList.add("hidden")
+            }
+        })
+            const goToTop = () => {
+            behavior: "smooth",
+            document.body.scrollIntoView();
+        };
+            backToTopButton.addEventListener("click", goToTop)
+            
+            
+            
+            const pageProgressBar = document.querySelector(".progress-bar1")
+
+
+            document.addEventListener("scroll", () => {
+            const scrolledPercentage =
+      (scrollContainer().scrollTop /
+        (scrollContainer().scrollHeight - scrollContainer().clientHeight)) *
+      100;
+  
+  pageProgressBar.style.width = `${scrolledPercentage}%`
+  
+  if (scrollContainer().scrollTop > showOnPx) {
+    backToTopButton.classList.remove("hidden");
+  } else {
+    backToTopButton.classList.add("hidden");
+  }
+});
