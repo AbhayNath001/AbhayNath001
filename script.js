a4e4b5cede203e31a646771da976483611d281d4
Abhay Nath <103759014+AbhayNath001@users.noreply.github.com>
2023-03-26 00:27:31 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -40,7 +40,7 @@ ScrollReveal({
 	duration: 2000,
 	delay: 200
 	});
-ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
-ScrollReveal().reveal('.home-img, .services-container, .mywork-box, .contact form', { origin: 'bottom' });
+ScrollReveal().reveal('.home-content, .heading, .checkbox-list, .skills', { origin: 'top' });
+ScrollReveal().reveal('.home-img, .services-container, .mywork-box, .contact form , .show_more', { origin: 'bottom' });
 ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
 ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
