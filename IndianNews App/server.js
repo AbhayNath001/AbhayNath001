3fd95c67553e2744f241a831db473a8b57262fbe
Abhay Nath <103759014+AbhayNath001@users.noreply.github.com>
2023-04-05 00:58:53 +0530

    Add files via upload

---
+++ IndianNews App/server.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const path = require('path')
+const axios = require('axios')
+const app = express()
+const port = 8080
+
+app.get('/', (req, res) => {
+  res.sendFile("index.html", { root: path.join(__dirname) } )
+})
+
+app.get('/api', async(req, res) => {
+  console.log(req._parsedUrl.query);
+  let url = "https://newsapi.org/v2/everything?" + req._parsedUrl.query
+  let r = await axios(url)
+  let a = r.data
+  res.json(a)
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
