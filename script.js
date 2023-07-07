ccf4b1378deb038099b8b59c8a2ccb06bb71a08b
Abhay Nath <103759014+AbhayNath001@users.noreply.github.com>
2023-07-07 22:04:12 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -34,13 +34,13 @@ window.onscroll = () => {
 	navbar.classList.remove('active');
 	};
 /*-------------------------------------------scroll reveal------------------------------------------------*/
-ScrollReveal({ 
-	// reset: true,
-	distance: '80px',
-	duration: 2000,
-	delay: 200
-	});
-ScrollReveal().reveal('.home-content, .heading, .checkbox-list, .skills', { origin: 'top' });
-ScrollReveal().reveal('.home-img, .services-container, .mywork-box, .contact form , .show_more', { origin: 'bottom' });
-ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
-ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
+// ScrollReveal({ 
+	//reset: true,
+	// distance: '80px',
+	// duration: 1500,
+	// delay: 200
+	// });
+// ScrollReveal().reveal('.home-content, .heading, .checkbox-list, .skills', { origin: 'top' });
+// ScrollReveal().reveal('.home-img, .services-container, .mywork-box, .show_more', { origin: 'bottom' });
+// ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
+// ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
