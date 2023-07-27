b4477ab44fdfbe96585458e0438600d611138a08
Abhay Nath <103759014+AbhayNath001@users.noreply.github.com>
2023-07-28 00:44:46 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -6,6 +6,73 @@ menuIcon.onclick = () => {
 	navbar.classList.toggle('active');
 };
 
+// Global variable to store the background color state
+var isBackgroundColorWhite = false;
+
+function changeBackgroundColor() {
+    var body = document.body;
+    var button = document.querySelector('.navbar i');
+	var nav = document.querySelector('.header');
+	var header_span = document.querySelector('.header span');
+	var services_box = document.querySelectorAll('.services-container .services-box');
+	var inputFields = document.querySelectorAll('.input-box input, textarea');
+	var headings = document.querySelectorAll('.mywork-layer h4');
+	var paragraphs = document.querySelectorAll('.mywork-layer p');
+
+    if (isBackgroundColorWhite) {
+        // Change from white to var(--bg-color)
+        body.style.backgroundColor = 'var(--bg-color)';
+		nav.style.backgroundColor = 'var(--bg-color)';
+        body.style.color = 'var(--text-color)';
+		header_span.style.color = '#0ef';
+		services_box.forEach(box => {
+            box.style.backgroundColor = '#1f242d';
+        });
+		inputFields.forEach(field => {
+			field.style.backgroundColor = '#1f242d';
+			field.style.color = 'white';
+		});
+		headings.forEach(heading => {
+			heading.style.color = 'black';
+		});
+		paragraphs.forEach(paragraph => {
+			paragraph.style.color = 'black';
+		});
+        button.style.color = 'var(--text-color)';
+        button.style.backgroundColor = 'var(--bg-color)';
+        document.documentElement.style.setProperty('--second-bg-color', '#323946');
+        document.documentElement.style.setProperty('--text-color', '#fff');
+        document.documentElement.style.setProperty('--main-color', '#0ef');
+        isBackgroundColorWhite = false;
+    } else {
+        // Change from var(--bg-color) to white
+        body.style.backgroundColor = '#edebeb';
+		nav.style.backgroundColor = '#edebeb';
+        body.style.color = 'black';
+		header_span.style.color = '#f24005';
+		services_box.forEach(box => {
+            box.style.backgroundColor = '#ccc9c8';
+        });
+		inputFields.forEach(field => {
+			field.style.backgroundColor = '#ccc9c8';
+			field.style.color = '#1f242d';
+		});
+		headings.forEach(heading => {
+			heading.style.color = 'white';
+		});
+		paragraphs.forEach(paragraph => {
+			paragraph.style.color = 'white';
+		});
+        button.style.color = 'black';
+        button.style.backgroundColor = '#c9c6c5';
+        document.documentElement.style.setProperty('--second-bg-color', '#e3e3e3');
+        document.documentElement.style.setProperty('--text-color', 'black');
+        document.documentElement.style.setProperty('--main-color', '#f24005');
+        isBackgroundColorWhite = true;
+    }
+}
+
+
 /*------------------------------------------scroll section active link-------------------------------------------------*/
 
 let sections = document.querySelectorAll('section');
